a5110b8119891e5e3f76efa7e3e41906d4e7f06f
John Wass <wassj@ctc.com>
2023-04-14 14:51:14 -0400

    wip

--- examples/feed.rs
+++ examples/feed.rs
@@ -8,6 +8,7 @@ use std::time::Duration;
 use clap::Parser;
 use crossbeam_channel::internal::SelectHandle;
 
+use audit_userspace_rs::auparse::feed::{Input, Output};
 use audit_userspace_rs::auparse::Feed;
 
 #[derive(Parser)]
@@ -19,21 +20,32 @@ struct Opts {
 fn main() -> Result<(), Box<dyn Error>> {
     let opts: Opts = Opts::parse();
 
-    let (_done, stream) = Feed::new().stream()?;
+    let (_done, h, stream) = Feed::new().stream()?;
     let rx = stream.rx.clone();
-    thread::spawn(move || loop {
+    let h = thread::spawn(move || {
+        let mut count = 0;
         for e in &rx {
-            println!("-- {e:?}");
+            match e {
+                Output::Done => break,
+                Output::Parsed(e) => {
+                    count += 1;
+                    println!("{e:?}")
+                }
+            }
         }
+        println!("output: {count}");
     });
 
     let f = File::open(opts.log).expect("failed to open file");
     let buff = BufReader::new(f);
 
     for line in buff.lines() {
-        if let Some(e) = stream.tx.send(format!("{}\n", line?)).err() {
+        if let Some(e) = stream.tx.send(Input::Raw(format!("{}\n", line?))).err() {
             println!("error: {e} {}", stream.tx.is_ready());
         }
     }
+    stream.tx.send(Input::Done)?;
+    h.join();
+
     Ok(())
 }
--- src/auparse/feed.rs
+++ src/auparse/feed.rs
@@ -1,5 +1,6 @@
 use std::ffi::{c_void, CStr};
 use std::ptr::NonNull;
+use std::thread::JoinHandle;
 use std::{ptr, thread};
 
 use crossbeam_channel::internal::SelectHandle;
@@ -12,11 +13,22 @@ use crate::auparse::error::Error;
 
 pub type Done = ();
 
+pub enum Input {
+    Raw(String),
+    Done,
+}
+
+pub enum Output {
+    Parsed(Entry),
+    Done,
+}
+
 #[derive(Clone)]
 pub struct Stream {
-    pub tx: Sender<String>,
-    pub rx: Receiver<Entry>,
-    e_tx: Sender<Entry>,
+    pub tx: Sender<Input>,
+    pub rx: Receiver<Output>,
+    count: u32,
+    e_tx: Sender<Output>,
 }
 
 pub struct Feed {
@@ -27,14 +39,13 @@ unsafe impl Send for Feed {}
 
 impl Drop for Feed {
     fn drop(&mut self) {
-        println!("==============--------------- DROPPING --------------================");
         unsafe {
             auparse_destroy(self.au.as_ptr());
         }
     }
 }
 
-pub type StreamRef = (Sender<Done>, Box<Stream>);
+pub type StreamRef = (Sender<Done>, JoinHandle<()>, Box<Stream>);
 
 impl Feed {
     pub fn new() -> Self {
@@ -44,23 +55,15 @@ impl Feed {
         }
     }
 
-    // consume the Feed
-    // set up the au with a callback
-    // start a thread that monitors the source, passing the input to the au
-    // the callback receives log entries and passes them to the sink
-    // the thread is closed and the au destroyed when the source closes
-    // when closing flushing is attempted if the sink is still valid
     pub fn stream(self) -> Result<StreamRef, Error> {
-        // source
         let (source_tx, source_rx) = crossbeam_channel::unbounded();
-        // sink
         let (sink_tx, sink_rx) = crossbeam_channel::unbounded();
-        // done
         let (done, shutdown) = crossbeam_channel::bounded(0);
 
         let stream = Box::new(Stream {
             tx: source_tx,
             rx: sink_rx,
+            count: 0,
             e_tx: sink_tx,
         });
 
@@ -69,33 +72,42 @@ impl Feed {
             auparse_add_callback(self.au.as_ptr(), Some(callback), user_data, Some(cleanup));
         }
 
-        thread::spawn(move || {
+        let h = thread::spawn(move || {
             let feed = self;
             let mut count = 0;
-            loop {
-                select! {
-                    recv(shutdown) -> _ => {
-                        println!("((((((((((((( SHUTTING DOWN ))))))))))))))))");
-                        break
-                    },
-                    recv(source_rx) -> e => if let Ok(txt)= e {
-                        count += 1;
-                        unsafe { auparse_feed(feed.au.as_ptr(), txt.as_ptr() as *const i8, txt.len().try_into().unwrap()); }
-                    },
+            'outer: loop {
+                for e in source_rx.recv() {
+                    match e {
+                        Input::Raw(txt) => {
+                            count += 1;
+                            unsafe {
+                                auparse_feed(
+                                    feed.au.as_ptr(),
+                                    txt.as_ptr() as *const i8,
+                                    txt.len().try_into().unwrap(),
+                                );
+                            }
+                        }
+                        Input::Done => {
+                            println!("input done");
+                            break 'outer;
+                        }
+                    }
                 }
             }
-            println!("handled {count} events");
+            println!("entries: {count}");
             unsafe {
-                //auparse_feed_age_events(feed.au.as_ptr());
+                auparse_feed_age_events(feed.au.as_ptr());
+                auparse_flush_feed(feed.au.as_ptr());
             }
-            println!("should drop the Feed");
+
+            println!("input handling thread is closed");
         });
 
-        Ok((done, stream))
+        Ok((done, h, stream))
     }
 }
 
-// the callback will
 // todo;; allow callback logic to be injected to the Feed prior to stream() being called
 unsafe extern "C" fn callback(
     au: *mut auparse_state_t,
@@ -109,12 +121,16 @@ unsafe extern "C" fn callback(
 
         let vp = user_data as *mut Stream;
         if let Some(e) = Entry::next(au) {
-            (*vp).e_tx.send(e);
+            (*vp).count += 1;
+            (*vp).e_tx.send(Output::Parsed(e));
         }
     }
 }
 
-extern "C" fn cleanup(_user_data: *mut ::std::os::raw::c_void) {
-    // todo;; send a Done event to the stream?
-    println!("========== callback cleanup ==========");
+extern "C" fn cleanup(user_data: *mut ::std::os::raw::c_void) {
+    unsafe {
+        let vp = user_data as *mut Stream;
+        (*vp).e_tx.send(Output::Done);
+        println!("valid: {}", (*vp).count);
+    }
 }
