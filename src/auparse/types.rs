3a62eb1a1b99da31ac715b779b0559e5c24b3aba
John Wass <wassj@ctc.com>
2023-04-20 07:39:12 -0400

    add fanotify type

--- src/auparse/types.rs
+++ src/auparse/types.rs
@@ -1,24 +1,25 @@
 use auparse_sys::*;
 
-// todo;; this can be autogen from lib/msg_typetab.h
+// todo;; this could maybe be autogen from lib/msg_typetab.h
 
 #[derive(Debug, PartialEq)]
 pub enum Type {
     Unknown(u32),
-    Syscall,
     Cwd,
+    Fanotify,
     Path,
     Proctitle,
+    Syscall,
     SystemBoot,
 }
 
 impl From<u32> for Type {
     fn from(v: u32) -> Self {
         use Type::*;
-
         match v {
             AUDIT_SYSCALL => Syscall,
             AUDIT_CWD => Cwd,
+            AUDIT_FANOTIFY => Fanotify,
             AUDIT_PATH => Path,
             AUDIT_PROCTITLE => Proctitle,
             AUDIT_SYSTEM_BOOT => SystemBoot,
@@ -30,14 +31,13 @@ impl From<u32> for Type {
 impl From<Type> for u32 {
     fn from(t: Type) -> Self {
         use Type::*;
-        print!("+");
-
         match t {
             Unknown(v) => v,
-            Syscall => AUDIT_SYSCALL,
             Cwd => AUDIT_CWD,
+            Fanotify => AUDIT_FANOTIFY,
             Path => AUDIT_PATH,
             Proctitle => AUDIT_PROCTITLE,
+            Syscall => AUDIT_SYSCALL,
             SystemBoot => AUDIT_SYSTEM_BOOT,
         }
     }
